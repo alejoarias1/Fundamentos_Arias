4723f0433d46a7619656de5ff65e4814268bef56
alejoarias1 <alearias519@gmail.com>
2023-04-08 18:25:54 -0300

    actualización practicas

---
+++ Prácticas/Intro_python_2.py
@@ -0,0 +1,40 @@
+#ejercicio 1
+def procedimiento (lista):
+    for i in lista :
+        if i == "control":
+            posicion = lista.index(i)
+            lista.insert(posicion,"revisado")
+            lista.remove (i)
+    print(lista)
+
+lista = ["a", "control", "c"]
+procedimiento (lista)
+
+#ejercicio 2
+def ejercicio2 (lista3):
+    lista3.pop(0)
+    print(lista3)
+
+lista3=[1,2,3]
+ejercicio2(lista3)
+
+#ejercicio 3
+def ejercicio3(elemento):
+    lista2=["e","y","a"]
+    for i in lista2:
+        return lista2.index(elemento)
+    
+print(ejercicio3("a"))
+
+#ejercicio 4
+def ejercicio4(lista4,lista5,elemento):
+    for i in lista4:
+        if i==elemento:
+            lista4.remove(i)
+            lista5.append(i)
+    print(lista4)
+    print(lista5)
+
+lista4=[1,2,3]
+lista5=[4,5,6]
+ejercicio4(lista4,lista5,1)
--- Prácticas/Manipulación_de_archivos.py
+++ Prácticas/Manipulación_de_archivos.py
@@ -3,15 +3,21 @@ import os
 #ejercicio 1
 contador=0
 with open ("a.txt","r") as mi_arch:
-    a=mi_arch.readline()
-    if a.startswith("P"):
-        contador+=1
+    for line in mi_arch:
+        if line.startswith("P"):
+            contador+=1
 print(contador)
 
 #ejercicio 2
 with open ("a.txt","r") as mi_arch2:
     for i in range(1):
         print(mi_arch2.readlines())
+
+#ejercicio 3
+def read_n_back_lines(n,archivo):
+    texto=open(archivo,"r").readlines()
+    for i in range ((len(texto) -n), len(texto)):
+        print(texto[i])
 
 #ejercicio 6
 with open ("salida.txt", "w") as salida:
@@ -20,3 +26,5 @@ with open ("salida.txt", "w") as salida:
             if i.endswith("\n"):
                 i.replace("\n"," ")
                 salida.write(i)
+
+#ejercicio 7
